683a3f55b9af5792b25bc932facf37651b85fdee
Katherine Luo <yluo985@aucklanduni.ac.nz>
2022-08-31 21:18:22 +1200

    Implementation of navbar for the page

---
+++ react-unique-airbnbs-v1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import './App.css';
+import GlobalStyle from './globalStyles';
+
+function App() {
+  return (
+    <>
+    <GlobalStyle/>
+    <Navbar />
+    </>
+  );
+}
+
+export default App;
